5628586f5bc1c6a2a7b846ec1a0223fbb8d88a73
ZakRab <107758804+ZakRab@users.noreply.github.com>
2025-02-27 14:47:17 -0600

    feat: implement routing and add new pages (Home, Login, Lobby, Game, Rules, Settings, About)

--- interview-day-game/src/App.jsx
+++ interview-day-game/src/App.jsx
@@ -2,15 +2,31 @@ import "./App.css";
 import { Button } from "@chakra-ui/react";
 import { AppContext } from "./context/useAppContext";
 import { useContext } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Lobby from "./pages/Lobby";
+import Game from "./pages/Game";
+import Rules from "./pages/Rules";
+import Settings from "./pages/Settings";
+import About from "./pages/About";
+
 function App() {
   const { theme, setTheme } = useContext(AppContext);
 
   return (
-    <>
-      <h1>{theme}</h1>
-      <input type="text" onChange={(e) => setTheme(e.target.value)} />
-      <Button>chuaoetuhsn</Button>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/lobby" element={<Lobby />} />
+        <Route exact path="/game" element={<Game />} />
+        <Route exact path="/rules" element={<Rules />} />
+        <Route exact path="/settings" element={<Settings />} />
+        <Route exact path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
