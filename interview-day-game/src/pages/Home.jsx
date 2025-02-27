dbd0f456b97df16fed1824070aaeb326fb6df879
ZakRab <107758804+ZakRab@users.noreply.github.com>
2025-02-27 15:24:51 -0600

    adds login page

--- interview-day-game/src/pages/Home.jsx
+++ interview-day-game/src/pages/Home.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Button } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+import { supabase } from "../supabase/supabaseClient";
 
 function Home() {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      <div>
+        <Button colorScheme="teal" size="lg" onClick={() => navigate("/login")}>
+          Login
+        </Button>
+        {/* nav to setings */}
+        <Button
+          colorScheme="teal"
+          size="lg"
+          onClick={() => navigate("/settings")}
+        >
+          Settings
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- interview-day-game/src/pages/Login.jsx
+++ interview-day-game/src/pages/Login.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { supabase } from "../supabase/supabaseClient";
+import { Button } from "@chakra-ui/react";
+export default function Login() {
+  const [session, setSession] = useState(null);
 
-function Login() {
-  return <div>Login</div>;
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (!session) {
+    return <Auth />;
+  } else {
+    return <div>Logged in!</div>;
+  }
 }
 
-export default Login;
+function Auth() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  async function signUpNewUser(email, password) {
+    const { data, error } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+      options: {
+        emailRedirectTo: "https://example.com/welcome",
+      },
+    });
+  }
+
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="email"
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      ></input>
+      <input
+        type="password"
+        placeholder="password"
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      ></input>
+      <Button onClick={() => signUpNewUser(email, password)} />
+    </>
+  );
+}
