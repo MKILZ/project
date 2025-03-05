6ada7ff160538334b06715702ebcbd1c59c5088d
marykatenussrallah <mnussrallah2@huskers.unl.edu>
2025-03-05 13:42:58 -0600

    msg

--- interview-day-game/src/pages/About.jsx
+++ interview-day-game/src/pages/About.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { TextBox } from 'devextreme-react/text-box';
+import MKHeadshot from "../assets/MKheadshot.jpg"
+import LucyHeadshot from "../assets/lucy_headshot.jpg"
+// TODO: get headshots for each group member
 
 function About() {
-  return <div>About</div>;
+  return <div>className="about"
+    <div> className="header"
+      <h1> About Us </h1>
+      <p> Who are we? </p>
+      <img>src={MKHeadshot}</img>
+      <TextBox />
+      <img>src={LucyHeadshot}</img>
+      {/* TODO: put in headshot for each group member */}
+    </div>
+
+
+
+
+
+  </div>;
 }
 
 export default About;
