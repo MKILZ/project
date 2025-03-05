4a4af08bd5253a566f40363131a7da9aeea9538c
ZakRab <107758804+ZakRab@users.noreply.github.com>
2025-03-05 14:03:35 -0600

    fix: correct JSX structure and remove unused TextBox component in About page

--- interview-day-game/src/pages/About.jsx
+++ interview-day-game/src/pages/About.jsx
@@ -1,25 +1,22 @@
 import React from "react";
-import { TextBox } from 'devextreme-react/text-box';
-import MKHeadshot from "../assets/MKheadshot.jpg"
-import LucyHeadshot from "../assets/lucy_headshot.jpg"
+import MKHeadshot from "../assets/MKheadshot.jpg";
+import LucyHeadshot from "../assets/lucy_headshot.jpg";
 // TODO: get headshots for each group member
 
 function About() {
-  return <div>className="about"
-    <div> className="header"
-      <h1> About Us </h1>
-      <p> Who are we? </p>
-      <img>src={MKHeadshot}</img>
-      <TextBox />
-      <img>src={LucyHeadshot}</img>
-      {/* TODO: put in headshot for each group member */}
+  return (
+    <div>
+      className="about"
+      <div>
+        className="header"
+        <h1> About Us </h1>
+        <p> Who are we? </p>
+        <img>src={MKHeadshot}</img>
+        <img>src={LucyHeadshot}</img>
+        {/* TODO: put in headshot for each group member */}
+      </div>
     </div>
-
-
-
-
-
-  </div>;
+  );
 }
 
 export default About;
