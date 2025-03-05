3d6bbdc3b073bdd0225361f11f47df99f1e1b166
ZakRab <107758804+ZakRab@users.noreply.github.com>
2025-03-05 14:09:44 -0600

    adds links to the about

--- interview-day-game/src/pages/About.jsx
+++ interview-day-game/src/pages/About.jsx
@@ -1,20 +1,23 @@
 import React from "react";
-import MKHeadshot from "../assets/MKheadshot.jpg";
-import LucyHeadshot from "../assets/lucy_headshot.jpg";
-// TODO: get headshots for each group member
 
 function About() {
   return (
     <div>
       className="about"
-      <div>
-        className="header"
-        <h1> About Us </h1>
-        <p> Who are we? </p>
-        <img>src={MKHeadshot}</img>
-        <img>src={LucyHeadshot}</img>
-        {/* TODO: put in headshot for each group member */}
-      </div>
+      <h1> About Us </h1>
+      <p> Who are we? </p>
+      <img
+        src={
+          "https://media.licdn.com/dms/image/v2/D4E03AQGs1UWJY1jRSg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1695250451964?e=1746662400&v=beta&t=r_bsLDZdeAw0MLaX5lBADgy-pU05KYcCk0fi4rqWUjI"
+        }
+        alt="MK Headshot"
+      />
+      <img
+        src={
+          "https://media.licdn.com/dms/image/v2/D5603AQH8y1jdo8U0hg/profile-displayphoto-shrink_400_400/B56ZSwRCtzHEAg-/0/1738124045816?e=1746662400&v=beta&t=lAT4IGTBu7XbKc-64mYp4KOVeX98ZEqxWV1I0qBq3r8"
+        }
+        alt="Lucy Headshot"
+      />
     </div>
   );
 }
