a0624051ee276a944e6785feb2455ff35a31a4f0
Ikuchar1 <ian1kuchar@gmail.com>
2025-03-05 13:42:30 -0600

    Created plain rules page - Needs some info added

--- interview-day-game/src/pages/Rules.jsx
+++ interview-day-game/src/pages/Rules.jsx
@@ -1,7 +1,196 @@
 import React from "react";
+import { Button, Container, Heading, Text, Box } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+
 
 function Rules() {
-  return <div>Rules</div>;
+  const navigate = useNavigate();
+  return (
+    <Container maxW="800px" py={6}>
+      <Heading as="h2" size="xl" mb={6} textAlign="Center">
+        Rules of Interview Day at Kauffman
+      </Heading>
+
+      <Text ml = {10}>
+          <strong>INSERT BETTER HEADER HERE?</strong>
+      </Text>
+
+      <Button colorScheme="teal" size="lg" ml = {5} mr = {5} onClick={() => navigate("/home")}>
+          Home
+        </Button>
+
+      <Button colorScheme="teal" size="lg" onClick={() => navigate("/login")}>
+          Login
+      </Button>
+
+
+      {/* Introduction */}
+      <Box mb={6} ma={6}>
+        <Heading as="h2" size="lg" mb={2} ml={3}>
+          Introduction
+        </Heading>
+        <Text ml={10}>
+          Each player will manage the role of a section of the Interview Day at the Raikes School of Computer Science and Management.
+          We have compressed the entire day into 4 differnt secitons so that you will be able to see how an average interview day works in under an hour of gameplay.
+
+          We will first go over the game board and give you basic instrucitons about gamepley. After that, you will spend an hour playing the game and we will have a short discussion about the game and how it relates to the Raikes School.
+        </Text>
+      </Box>
+      
+      <Box mb={6} ma={6}>
+        <Heading as="h2" size="lg" mb={2} ml={3} ma={6}>
+          Flow
+        </Heading>
+        <Text ml={10}>
+          During this game you will see potential students arrive in your departments each simulated hour, and you will be tasked with making decisions on how to best manage the students and volunteers in your section.
+
+          First, notice how some sections have arrows points to them from the edge of the board, this represents students arriving at your section at the start of the day.
+
+          All of the other secitons will have arrows pointing to and from their departments, this represents students moving from one section to another.
+
+          Players are only allowed to move to other departments that have arrows pointing to them from their department. This is to simulate the flow of students through the Raikes School on Interview Day.
+
+          There will be an arrival display on your screen "INSERT PICTURE OF ARRIVAL DISPLAY HERE". This is your time clock during the interview day and will show you which hour you are on. This is also where you will be able to see how many more students will be arriving at each department at the start of this hour.
+        </Text>
+
+      </Box>
+
+      <Box mb={6}>
+        <Heading as="h2" size="lg" mb={2} ml={3} ma={6}>
+          Beads
+        </Heading>
+        <Text ml={10}>
+          There are three different beads you will use throughout the next hour.
+          The blue beads are potential students that need to be guided through the interview day. Each department will start with a certain number of students in their section.
+
+          The white beads represent the current Raikes School volunteers helping out. Each department will start with a certain number of volunteers in their section.
+
+          As you look at the board "INSERT BOARD IMAGE HERE", you will see that each student is currently paired with a Raikes School volunteer. This is to simulate the students being guided through the interview day by a current Raikes School student.
+          You will also see each section has some extra volunteers that are not assigned to a students. These volunteers are available for students care and are there to accommodate additional students who may arrive.
+
+          The ratio of volunteers to students must remain 1:1. If you have more students than volunteers, you will need to call in extra volunteers to help out. 
+          If you do not have enough volunteers to match with all of the students in your section you may choose to call in extra volunteers to help.
+
+          These extra volunteers will be represented by a clear bead. You may only call in extra volunteers once per hour and you must wait one hour before you can match them with a student.
+        </Text>
+      </Box>
+
+      <Box mb={6}>
+        <Heading as="h2" size="lg" mb={2} ml={3} ma={6}>
+          Steps
+        </Heading>
+        <Text ml={10}>
+          The best way for you to learn how to play the game is to understand the 5 steps you will take each turn. Below are the 5 integral steps you will take.
+
+        </Text>
+        
+        <Heading as="h3" size="md" mb={2} ml={3} ma={6}>
+          Step 1: Arrivals
+        </Heading>
+        <Text ml={10}>
+          To start your turn you will manage new students arriving. Look at the arrival display on your screen to see how many students will be arriving at your section this hour.
+
+          Focus on the number of students arriving at your department. Go ahead and place that mean blue beads in the arrow pointing to your department. This represents the students arriving at your section.
+
+          Now, if you have the volunteer capacity in your department - represented by the white beads that are not with another student - you can match the students with a volunteer right away. 
+
+          On the first turn everyone should have enough extra volunteers to manage the new students arriving.
+
+          If you do not have enough volunteers to match incoming students, then leave that student on the arrival arrow for now.
+        </Text>
+
+        <Heading as="h3" size="md" mb={2} ml={3} ma={6}>
+          Step 2: Exits
+        </Heading>
+        <Text ml={10}>
+          Next, you will manage exit students from your department. Each department will have a different way for students to exit their department.
+
+          First, Welcome Session "FILL IN WITH BOARD INFO"
+
+          Next, Lunch "FILL IN WITH BOARD INFO"
+
+          Next, Interview "FILL IN WITH BOARD INFO"
+
+          Finally, Presentations "FILL IN WITH BOARD INFO"
+
+          "PREVIOUS INFO IS ON PAGE 16 OF THE RULES (STEP 7)"
+        </Text>
+
+        <Heading as="h3" size="md" mb={2} ml={3} ma={6}>
+          Step 3: Closed?
+        </Heading>
+        <Text ml={10}>
+          Next, you all have to decide if your department will be closed for this hour.
+
+          The closed decision allows you to signigy that you are full and cannot take more students at this time - or that you just don't choose to take more students at this time. You can signify that you are closed by pressing the "CLOSED" button by your section"
+          "INSERT PICTURE OF CLOSED BUTTON HERE"
+
+          You may do this anytime during the hour, and you may uncheck it at anytime as well.
+
+          "INSERT CLOSING INFO FOR THE EMERGENCY ROOM/INTERVIEW?"
+        </Text>
+
+        <Heading as="h3" size="md" mb={2} ml={3} ma={6}>
+          Step 4: Staffing?
+        </Heading>
+        <Text ml={10}>
+          The other decision you have to make each hour is staffing your department. If you want to call in extra volunteers to help out, here is the procedure. Notice the place in each department labeled "Extra Volunteers Called" "INSERT PIC HERE".
+
+          If you want to call in one or more volunteers, just take the clear beads and place them in the "Extra Volunteers Called" area.
+
+          Then at the start of the NEXT hour, you can move those cleaer beads into care to accommodate new students. Just like in the real world, it will take about an hour for extra volunteers to arrive, so you must wait nutil the start of the next hour to use those Extra Staff beads.
+
+          By the way, you can send Extra volunteers home at any time during the game play if you no longer have a need for them.
+        </Text>
+
+        <Heading as="h3" size="md" mb={2} ml={3} ma={6}>
+          Step 5: Paperwork
+        </Heading>
+        <Text ml={10}>
+          The final step each hour is Paperwork, which each department manager will fill out. Find the paperwork form on the side of the gameplay screen. "INSERT PIC HERE" 
+          
+          At the end of each hour you will count and record the number of students you have waiting on the arrivals arrow leaading into your department.
+
+          For this hour each section except for "FILL IN" should log 0 for the number of students waiting to enter, while "FILL IN" should log 1 in that space.
+
+          You also have a palce on your form for documenting the number of Requests Waiting, which means the count of Request cards that are waiting to move into your departments.
+
+          Finally, you each have a place on your form for accounting for Extra Volunteers. For this item, at the end of each hour, just count any clear beads anywhere within your department and log that number on the form.
+
+          A useful point about these steps each hour: they do not have to be done in order. For example, if you have students leaving your section during an hour, you may jsut go ahread and move any new arrivals waiting into care-as long as you wait the minimum hour delay for inter-department transfers.
+        </Text>
+
+      </Box>
+
+      <Box mb={6} ma={6}>
+        <Heading as="h2" size="lg" mb={2} ml={3}>
+          Performance Measures
+        </Heading>
+        <Text ml={10}>
+          Your performance will be measured during the game in three areas: the quality of service that you deliver, your financial perforance, and your ability to complete the game play within our allotted time. 
+
+          You will affect financial performance when you cause patients to be turned away form the Raikes School(and you therefore lose revenue) or when you call in extra volunteers (and you therefore incur additional costs).
+
+          You will accumulate qualiuty erros when you keep stude ts waiting too long, when you divert students and if you use too many extra volunteers.
+
+          Your ability to complete the game play will require you to move along thorugh steps and hours at a reasonale pace, within the time most groups require. There will periodically be announcements about how much time you have left to complete the game.
+
+        </Text>
+      </Box>
+
+      <Box mb={6} ma={6}>
+        <Heading as="h2" size="lg" mb={2} ml={3} >
+          Begin the Game
+        </Heading>
+        <Text ml={10}>
+          Now that you have a basic understanding of the game, we will begin the game. You will have 1 hour to play the game and then we will have a short discussion about the game and how it relates to the Raikes School
+          . Make sure to follow all of the steps and make sure to have fun!
+        </Text>
+
+      </Box>
+      
+    </Container>
+  );
 }
 
 export default Rules;
