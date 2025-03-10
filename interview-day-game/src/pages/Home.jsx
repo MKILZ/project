5181d25fd25bc5069378645f03ad70a5a51d9e5c
ZakRab <107758804+ZakRab@users.noreply.github.com>
2025-03-10 17:25:41 -0500

    add create game button Co-authored-by: Ian Kuchar <ian1kuchar@gmail.com>

--- interview-day-game/src/pages/Home.jsx
+++ interview-day-game/src/pages/Home.jsx
@@ -1,114 +1,137 @@
-import { Flex, Heading, HStack, IconButton, Input, Stack } from "@chakra-ui/react";
+import {
+  Flex,
+  Heading,
+  HStack,
+  IconButton,
+  Input,
+  Stack,
+} from "@chakra-ui/react";
 import { FiSettings } from "react-icons/fi";
 import React from "react";
 import { Button } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../supabase/supabaseClient";
-
+import { useState } from "react";
 function Home() {
   const navigate = useNavigate();
+  const [lobby, setLobby] = useState(0);
   return (
-    <div style={{ backgroundColor: '#622A2A', width: '100vw', height: '100vh' }}>
+    <div
+      style={{ backgroundColor: "#622A2A", width: "100vw", height: "100vh" }}
+    >
       <div>
-        <Stack 
-          align="center">
-            <Flex 
-              direction="row">
-                <Flex
-                  direction="column"
-                  order="1"
-                  justify="center">
-                {/* Heading */}
-                  <Flex
-                    order="1"
-                    justify="center">
-                  <Heading 
-                    fontSize={100} 
-                    fontFamily="inter" 
-                    align="center"
-                    padding={50}
-                    colorPalette='white'>
-                      Interview Day
-                  </Heading>
-                  </Flex>
-                  <Flex
-                  order="2"
-                  justify="center">
-                  <Heading 
-                    fontSize={100} 
-                    fontFamily="inter" 
-                    align="center" 
-                    padding={50}
-                    colorPalette="white">
-                      At
-                  </Heading>
-                  </Flex>
-                  <Flex
-                  order="3"
-                  justify="center">
-                  <Heading 
-                    fontSize={100} 
-                    fontFamily="inter" 
-                    align="center" 
-                    padding={50}
-                    colorPalette="white">
-                      The Raikes School
-                  </Heading>
-                  </Flex>
-                </Flex>
-                    
-                {/* nav to setings */}
-                <Flex
-                  order="2"
-                  justify="flex-end"
-                  gap="80">
-                    <IconButton 
-                      aria-label="Settings"
-                      onClick={() => navigate("/settings")}
-                      rounded='full'
-                      size='xl'
-                      colorPalette="white">
-                        <FiSettings />
-                    </IconButton>
-                </Flex>
+        <Stack align="center">
+          <Flex direction="row">
+            <Flex direction="column" order="1" justify="center">
+              {/* Heading */}
+              <Flex order="1" justify="center">
+                <Heading
+                  fontSize={100}
+                  fontFamily="inter"
+                  align="center"
+                  padding={50}
+                  colorPalette="white"
+                >
+                  Interview Day
+                </Heading>
+              </Flex>
+              <Flex order="2" justify="center">
+                <Heading
+                  fontSize={100}
+                  fontFamily="inter"
+                  align="center"
+                  padding={50}
+                  colorPalette="white"
+                >
+                  At
+                </Heading>
+              </Flex>
+              <Flex order="3" justify="center">
+                <Heading
+                  fontSize={100}
+                  fontFamily="inter"
+                  align="center"
+                  padding={50}
+                  colorPalette="white"
+                >
+                  The Raikes School
+                </Heading>
+              </Flex>
             </Flex>
 
+            {/* nav to setings */}
+            <Flex order="2" justify="flex-end" gap="80">
+              <IconButton
+                aria-label="Settings"
+                onClick={() => navigate("/settings")}
+                rounded="full"
+                size="xl"
+                colorPalette="white"
+              >
+                <FiSettings />
+              </IconButton>
+            </Flex>
+          </Flex>
+
           {/* Room Code Input */}
-          <HStack 
-            spacing={10}
-            paddingTop={20}
-            paddingBottom={0}>
-            <Input 
-              size="2xl" 
-              placeholder="Enter The Room Code" 
+          <HStack spacing={10} paddingTop={20} paddingBottom={0}>
+            <Input
+              size="2xl"
+              placeholder="Enter The Room Code"
               maxWidth={250}
               maxHeight={50}
-              variant='filled'
-              colorPalette="white"/>
+              variant="filled"
+              colorPalette="white"
+            />
 
-            <Button 
+            <Button
               colorPalette="green"
-               size="xl"
-               rounded="md"
-               variant="subtle"
-               onClick={() => navigate("/game")}>
+              size="xl"
+              rounded="md"
+              variant="subtle"
+              onClick={() => navigate("/game")}
+            >
               Join Room
             </Button>
           </HStack>
 
-          <Heading 
-            size="5xl"
-            padding={10}>
+          <Heading size="5xl" padding={10}>
             Or
           </Heading>
 
           {/* nav to login */}
-          <Button  
-            size="xl" 
-            onClick={() => navigate("/login")}
-            colorPalette="white">
+          {supabase.auth.getUser != null ? (
+            <Button
+              size="xl"
+              onClick={() => navigate("/login")}
+              colorPalette="white"
+            >
               Login as a Host
-          </Button>
+            </Button>
+          ) : (
+            <HStack spacing={10} paddingTop={20} paddingBottom={0}>
+              <Input
+                size="2xl"
+                placeholder="Enter The Room Code"
+                maxWidth={250}
+                maxHeight={50}
+                variant="filled"
+                colorPalette="white"
+                value={lobby}
+                onChange={(e) => setLobby(e.target.value)}
+              />
+
+              <Button
+                colorPalette="green"
+                size="xl"
+                rounded="md"
+                variant="subtle"
+                onClick={() => navigate("/lobby/" + lobby)}
+              >
+                Create Room
+              </Button>
+            </HStack>
+          )}
         </Stack>
       </div>
     </div>
