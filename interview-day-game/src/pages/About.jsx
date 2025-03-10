e88efc4c6acd0f7d3c5927f37751d8efef270e74
marykatenussrallah <mnussrallah2@huskers.unl.edu>
2025-03-10 12:08:07 -0500

    committing

--- interview-day-game/src/pages/About.jsx
+++ interview-day-game/src/pages/About.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Text } from "@chakra-ui/react"
 
 function About() {
   return (
@@ -11,12 +12,29 @@ function About() {
         }
         alt="MK Headshot"
       />
+      <Text> Mary Kate is a sophomore biochemistry major in the Raikes School from Omaha, NE. </Text>
       <img
         src={
           "https://media.licdn.com/dms/image/v2/D5603AQH8y1jdo8U0hg/profile-displayphoto-shrink_400_400/B56ZSwRCtzHEAg-/0/1738124045816?e=1746662400&v=beta&t=lAT4IGTBu7XbKc-64mYp4KOVeX98ZEqxWV1I0qBq3r8"
         }
         alt="Lucy Headshot"
       />
+      <Text> Lucy is a sophomore computer science major in the Raikes School from Elko, NV. </Text>
+
+      <img
+        src={"https://media.licdn.com/dms/image/v2/D5603AQEPQAt1bXEh-A/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1694104002893?e=1746662400&v=beta&t=d0pLDh1XINoJPW3S1m-xkZnkvh6yrZTgTLEeBE5KuQc"
+
+        }
+        alt="Ian Headshot"
+      />
+      <Text> Ian is a sophomore computer science major in the Raikes School from Bloomfield, NE. </Text>
+
+      <img 
+        src={""}
+        alt="Zak Headshot"
+      />
+      <Text> Zak is a sophomore computer science major in the Raikes School from Omaha, NE. </Text>
+
     </div>
   );
 }
