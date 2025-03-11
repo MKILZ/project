8fae2408df36f499c3dc5563adde39c16a292133
ZakRab <107758804+ZakRab@users.noreply.github.com>
2025-03-10 20:44:34 -0500

    refactor: Uncomment presence event handlers in Lobby component

--- interview-day-game/src/pages/Lobby.jsx
+++ interview-day-game/src/pages/Lobby.jsx
@@ -4,20 +4,29 @@ import { supabase } from "../supabase/supabaseClient";
 function Lobby() {
   const { lobby } = useParams();
   const room = supabase.channel(lobby);
-  useEffect(() => {
-    room
-      .on("presence", { event: "sync" }, () => {
-        const newState = room.presenceState();
-        console.log("sync", newState);
-      })
-      .on("presence", { event: "join" }, ({ key, newPresences }) => {
-        console.log("join", key, newPresences);
-      })
-      .on("presence", { event: "leave" }, ({ key, leftPresences }) => {
-        console.log("leave", key, leftPresences);
-      })
-      .subscribe();
-  }, []);
+  // useEffect(() => {
+  room
+    .on("presence", { event: "sync" }, () => {
+      const newState = room.presenceState();
+      console.log("sync", newState);
+    })
+    .on("presence", { event: "join" }, ({ key, newPresences }) => {
+      console.log("join", key, newPresences);
+    })
+    .on("presence", { event: "leave" }, ({ key, leftPresences }) => {
+      console.log("leave", key, leftPresences);
+    })
+    .subscribe();
+  // }, []);
+
+  function messageReceived(payload) {
+    console.log(payload);
+  }
+
+  // Subscribe to the Channel
+  channelA
+    .on("broadcast", { event: "test" }, (payload) => messageReceived(payload))
+    .subscribe();
 
   return (
     <div>
