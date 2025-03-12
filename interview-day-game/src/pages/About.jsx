27a85a0939f9b342c5b61e8a25aa3b4f49e393ad
marykatenussrallah <mnussrallah2@huskers.unl.edu>
2025-03-11 21:48:34 -0500

    got all info on about pages

--- interview-day-game/src/pages/About.jsx
+++ interview-day-game/src/pages/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Text } from "@chakra-ui/react"
-import { Image } from "@chakra-ui/react"
-import { Heading } from "@chakra-ui/react"
+import { Text, Image, Heading, Stack, Box } from "@chakra-ui/react"
+import zakrab from "../../public/zakrab.png"
+
 
 
 function About() {
@@ -77,11 +77,10 @@ function About() {
             Zak Rab
           </Heading>
           <Image
-            src={"https://drive.google.com/file/d/12tQaswIam1EJubX429nnNQaeg-g7BmVY/view"
-            }
+            src="/zakrab.png"
             boxSize="200px"
             borderRadius="full"
-            fit="cover"
+            objectFit="cover"
             alt="Zak Headshot"
           />
         <Text textStyle="md"> 
@@ -97,58 +96,6 @@ function About() {
 
         With Interview Day at the Raikes School, we hope to recreate the same sense of challenge, camaraderie, and discovery that we experienced. Our goal is for you to walk away having learned as much as we did—about problem-solving, teamwork, and maybe even yourself. Thank you for playing, and welcome to an experience that has meant so much to us. 
         </Text>
-
-        {/* <Image
-          // height="200px"
-          src={"https://media.licdn.com/dms/image/v2/D5603AQEPQAt1bXEh-A/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1694104002893?e=1746662400&v=beta&t=d0pLDh1XINoJPW3S1m-xkZnkvh6yrZTgTLEeBE5KuQc"
-          }
-          boxSize="200px"
-          borderRadius="full"
-          fit="cover"
-          alt="Ian Headshot"
-        />
-        <Text> Ian is a sophomore computer science major in the Raikes School from Bloomfield, NE. </Text> */}
-        {/* <Image
-          // height="200px"
-          src={
-            "https://media.licdn.com/dms/image/v2/D4E03AQGs1UWJY1jRSg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1695250451964?e=1746662400&v=beta&t=r_bsLDZdeAw0MLaX5lBADgy-pU05KYcCk0fi4rqWUjI"
-          }
-          boxSize="200px"
-          borderRadius="full"
-          fit="cover"
-          alt="MK Headshot"
-        />
-        <Text> Mary Kate is a sophomore biochemistry major in the Raikes School from Omaha, NE. </Text>
-        <Image */}
-          // height="200px"
-          {/* src={
-            "https://media.licdn.com/dms/image/v2/D5603AQH8y1jdo8U0hg/profile-displayphoto-shrink_400_400/B56ZSwRCtzHEAg-/0/1738124045816?e=1746662400&v=beta&t=lAT4IGTBu7XbKc-64mYp4KOVeX98ZEqxWV1I0qBq3r8"
-          }
-          boxSize="200px"
-          borderRadius="full"
-          fit="cover"
-          alt="Lucy Headshot"
-        />
-        <Text> Lucy is a sophomore computer science major in the Raikes School from Elko, NV. </Text>
-
-        <Image
-          // height="200px"
-          src={"https://drive.google.com/file/d/12tQaswIam1EJubX429nnNQaeg-g7BmVY/view"}
-          boxSize="200px"
-          borderRadius="full"
-          fit="cover"
-          alt="Zak Headshot"
-        />
-        <Text> Zak is a sophomore computer science major in the Raikes School from Omaha, NE. </Text> */}
-
-        {/* <h2> Why we are doing this </h2>
-        <Text> 
-        As college freshmen, the four of us participated in a team-building exercise that left a lasting impact. It was more than just a game—it challenged us, pushed us to think critically, and helped us grow both as individuals and as a team. Through that experience, we gained invaluable lessons about communication, problem-solving, and collaboration.
-
-        Now, as students who have come so far in our journey at the Raikes School, we wanted to create something that not only pays tribute to the game that shaped us but also to the place where we first played it. The Raikes School has been instrumental in our growth, and we owe part of our development to that very first exercise.
-
-        With Interview Day at the Raikes School, we hope to recreate the same sense of challenge, camaraderie, and discovery that we experienced. Our goal is for you to walk away having learned as much as we did—about problem-solving, teamwork, and maybe even yourself. Thank you for playing, and welcome to an experience that has meant so much to us. 
-        </Text> */}
 
       </div>
     </div>
