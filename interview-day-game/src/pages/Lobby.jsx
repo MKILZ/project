7ba3e23e4b9753cc388e24d9c6f9f2fbb6cb5f34
marykatenussrallah <mnussrallah2@huskers.unl.edu>
2025-05-06 14:32:40 -0500

    added music to lobby

--- interview-day-game/src/pages/Lobby.jsx
+++ interview-day-game/src/pages/Lobby.jsx
@@ -1,11 +1,14 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState, useContext, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { supabase } from "../supabase/supabaseClient";
 import { AppContext } from "../context/useAppContext";
 import { useNavigate } from "react-router-dom";
 import Carousel from "react-bootstrap/Carousel";
+import backgroundMusic from "/Users/marykatenussrallah/project/interview-day-game/src/assets/Local Elevator - Kevin MacLeod.mp3";
+
 
 function Lobby() {
+  const audioRef = useRef(null);
   const { lobby } = useParams();
   const { activeUser, players, setPlayers } = useContext(AppContext);
   const navigate = useNavigate();
@@ -29,6 +32,12 @@ function Lobby() {
   }
 
   useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.volume = 0.3
+      audioRef.current.play().catch((e) => {
+        console.warn("Autoplay failed:", e);
+      });
+    }
     if (!activeUser) {
       navigate("/");
     }
@@ -85,68 +94,73 @@ function Lobby() {
       .subscribe();
   }, [lobby]);
   return (
-    <div className="d-flex justify-content-center h-100 w-50 gap-5 mx-auto">
-      <div className="card p-5 bg-gray-100 mt-5">
-        <div className="d-flex justify-content-between">
-          <h1
-            className=""
-            onClick={
-              //onclick copy the lobby code to clipboard
-              () => {
-                copyTextToClipboard(lobby);
+    <>
+      <audio ref={audioRef} loop autoPlay>
+        <source src={backgroundMusic} type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
+      <div className="d-flex justify-content-center h-100 w-50 gap-5 mx-auto">
+        <div className="card p-5 bg-gray-100 mt-5">
+          <div className="d-flex justify-content-between">
+            <h1
+              className=""
+              onClick={
+                //onclick copy the lobby code to clipboard
+                () => {
+                  copyTextToClipboard(lobby);
+                }
               }
-            }
-          >
-            Lobby: {lobby}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-copy"
-              viewBox="0 0 16 16"
             >
-              <path
-                fillRule="evenodd"
-                d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"
-              />
-            </svg>
-          </h1>
+              Lobby: {lobby}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-copy"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"
+                />
+              </svg>
+            </h1>
 
-          {activeUser.role === "Host" ? (
-            <button
-              // disabled={players.length !== 4} // uncomment in in production
-              className="btn btn-secondary"
-              onClick={() => {
-                startGame();
-                navigate("/game/" + lobby);
-              }}
-            >
-              Start Game! {players.length === 4 ? "" : players.length + "/4"}
-            </button>
-          ) : (
-            <h3>{players.length === 4 ? "" : players.length + "/4"}</h3>
-          )}
-        </div>
-        <div className="d-flex gap-3 pt-5">
-          <div className="d-flex flex-column gap-3 " style={{ width: "18rem" }}>
-            {players.map((player, idx) => {
-              return (
-                <div className="card p-3 d-flex">
-                  <div>{idx + 1 + " " + player}</div>
-                  <div></div>
-                </div>
-              );
-            })}
+            {activeUser.role === "Host" ? (
+              <button
+                // disabled={players.length !== 4} // uncomment in in production
+                className="btn btn-secondary"
+                onClick={() => {
+                  startGame();
+                  navigate("/game/" + lobby);
+                }}
+              >
+                Start Game! {players.length === 4 ? "" : players.length + "/4"}
+              </button>
+            ) : (
+              <h3>{players.length === 4 ? "" : players.length + "/4"}</h3>
+            )}
           </div>
-          <div id="carouselExample" className="card" style={{ width: "30rem" }}>
-            <CarouselComp></CarouselComp>
-            <div className="card-body">
-              <h5 className="card-title">Character roster</h5>
-              <p className="card-text">
-                Each character has a unique role played in the interview day.
-              </p>
-              {/* <div
+          <div className="d-flex gap-3 pt-5">
+            <div className="d-flex flex-column gap-3 " style={{ width: "18rem" }}>
+              {players.map((player, idx) => {
+                return (
+                  <div className="card p-3 d-flex">
+                    <div>{idx + 1 + " " + player}</div>
+                    <div></div>
+                  </div>
+                );
+              })}
+            </div>
+            <div id="carouselExample" className="card" style={{ width: "30rem" }}>
+              <CarouselComp></CarouselComp>
+              <div className="card-body">
+                <h5 className="card-title">Character roster</h5>
+                <p className="card-text">
+                  Each character has a unique role played in the interview day.
+                </p>
+                {/* <div
                 href="#"
                 className="btn btn-secondary"
                 onClick={() => {
@@ -155,11 +169,12 @@ function Lobby() {
               >
                 Ready Up!
               </div> */}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
