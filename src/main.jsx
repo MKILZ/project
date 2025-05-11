89719ff5f467ab15339124c4f0acf1c25d1dd603
ZakRab <107758804+ZakRab@users.noreply.github.com>
2025-05-10 21:50:34 -0500

    feat: update main entry point to use JSX and enhance background styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { AppProvider } from "./context/useAppContext.jsx";
@@ -6,10 +5,10 @@ import { AppProvider } from "./context/useAppContext.jsx";
 import "./index.css";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root")).render(
   <>
-      <AppProvider>
-        <App />
-      </AppProvider>
+    <AppProvider>
+      <App />
+    </AppProvider>
   </>
 );
